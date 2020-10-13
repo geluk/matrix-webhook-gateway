cdd36badf781aff2d6a4245fce8dff49b25fc6eb
Johan Geluk <johan@geluk.io>
2020-10-13 13:49:14 +0200

    Create template for homeserver.db

--- synapse-dev/start-docker.sh
+++ synapse-dev/start-docker.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ ! -e "synapse-data/homeserver.db" ]; then
+    echo "Copying homeserver.db from template"
+    cp template/homeserver.db synapse-data/homeserver.db
+fi
+
 export SYNAPSE_UID="$(id -u)"
 export SYNAPSE_GID="$(id -g)"
 
