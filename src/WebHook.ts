794d49c5bae5ae73e32da6e0d68d5b52329753f0
Johan Geluk <johan@geluk.io>
2020-10-13 11:12:30 +0200

    Split appservice and webhook

---
+++ src/WebHook.ts
@@ -0,0 +1,32 @@
+
+import * as express from "express";
+
+export default class WebHook {
+
+    public constructor() {
+        const app = express();
+
+        app.get("*", (rq, rs, next) => {
+            console.log(`${rq.method} ${rq.url}`);
+            console.log(rq.body);
+            rs.contentType("application/json").send("{}");
+        })
+        
+        app.post("*", (rq, rs, next) => {
+            console.log(`${rq.method} ${rq.url}`);
+            console.log(rq.body);
+            rs.contentType("application/json").send("{}");
+        })
+        
+        app.put("*", (rq, rs, next) => {
+            console.log(`${rq.method} ${rq.url}`);
+            console.log(rq.body);
+            rs.contentType("application/json").send("{}");
+        })
+        
+        app.listen(8020, () => {
+            console.log("Web server ready.");
+        });
+    }
+}
+
