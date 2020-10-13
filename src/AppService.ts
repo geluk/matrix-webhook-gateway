026e8f73e839f6e148c833a05a822ca97988b3e3
Johan Geluk <johan@geluk.io>
2020-10-13 11:56:04 +0200

    Create AppService handler methods

--- src/AppService.ts
+++ src/AppService.ts
@@ -5,64 +5,88 @@ import { Cli, Bridge, AppServiceRegistration, MatrixUser, WeakEvent, Request, Br
 
 export default class AppService {
 
+    private bridge: Bridge;
+    private cli: Cli<Record<string, unknown>>;
+
     private constructor() {
-    }
-
-    static create(): AppService {
-        let appservice = new AppService();
-        new Cli({
-            registrationPath: "",
-            generateRegistration: function(reg: AppServiceRegistration, callback) {
-                reg.setId(AppServiceRegistration.generateToken());
-                reg.setHomeserverToken(AppServiceRegistration.generateToken());
-                reg.setAppServiceToken(AppServiceRegistration.generateToken());
-                reg.setSenderLocalpart("webhook");
-                reg.addRegexPattern("users", "@hook_.*", true);
-                callback(reg);
-            },
-            run: appservice.run
-        }).run();
-        return appservice;
-    }
-
-    private run(port: number, config: Record<string, unknown>) {
-        let bridge = new Bridge({
+        this.bridge = new Bridge({
             homeserverUrl: "http://127.0.0.1:8008",
             domain: "matrix.local",
             registration: "registration.yaml",
             disableStores: true,
             controller: {
-                onUserQuery: function(user: MatrixUser): Object {
-                    console.log(`User provision requested: ${user.localpart}:${user.host}`);
-                    return {}
-                },
-                onEvent: function(request: Request<WeakEvent>, context: BridgeContext) {
-                    let event = request.getData();
-                    if (event.type === "m.room.message") {
-                        console.log(`New message: ${event.content.body}`);
-                        if (event.sender != bridge.getBot().getUserId()) {
-                            bridge.getIntent().sendMessage(event.room_id, {
-                                msgtype: "m.text",
-                                body: "PONG"
-                            });
-                        }
-                    } else if (event.type === "m.room.member" && event.content.membership === "invite") {
-                        console.log(`${event.state_key} was invited to ${event.room_id}`);
-                        if (event.state_key === bridge.getBot().getUserId()) {
-                            console.log(`Accepting invite.`);
-                            bridge.getIntent(event.state_key)
-                                .join(event.room_id);
-                        }
-                    }else {
-                        console.log(`Event ignored: ${event.type}`);
-                        return;
-                    }
-                    return;
-                },
+                onUserQuery: this.handleUserQuery.bind(this),
+                onLog: this.handleLog.bind(this),
+                onEvent: this.handleEvent.bind(this),
             }
         });
+        this.cli = new Cli({
+            registrationPath: "",
+            generateRegistration: this.generateRegistration,
+            run: this.onStart.bind(this),
+        })
+        console.log(`Bridge ready`);
+    }
+
+    static create(): AppService {
+        let appservice = new AppService();
+        appservice.start();
+        return appservice;
+    }
+    
+    private start() {
+        this.cli.run();
+    }
+
+    private onStart(port: number, config: Record<string, unknown>) {
         console.log(`Ready on port ${port}`);
-        bridge.run(8023, config);
+        this.bridge.run(8023, config);
+    }
+
+    private handleUserQuery(user: MatrixUser): Object {
+        console.log(`User provision requested: ${user.localpart}:${user.host}`);
+        return {};
+    }
+
+    private handleLog(text: string, isError: boolean){
+        if (isError) {
+            console.log(`[appservice] ${text}`);
+        } else {
+            console.error(`[appservice] ${text}`);
+        }
+    }
+
+    private generateRegistration(reg: AppServiceRegistration, callback) {
+        reg.setId(AppServiceRegistration.generateToken());
+        reg.setHomeserverToken(AppServiceRegistration.generateToken());
+        reg.setAppServiceToken(AppServiceRegistration.generateToken());
+        reg.setSenderLocalpart("webhook");
+        reg.addRegexPattern("users", "@hook_.*", true);
+        callback(reg);
+    }
+
+    private handleEvent(request: Request<WeakEvent>, context: BridgeContext) {
+        let event = request.getData();
+        if (event.type === "m.room.message") {
+            console.log(`New message: ${event.content.body}`);
+            if (event.sender != this.bridge.getBot().getUserId()) {
+                this.bridge.getIntent().sendMessage(event.room_id, {
+                    msgtype: "m.text",
+                    body: "PONG"
+                });
+            }
+        } else if (event.type === "m.room.member" && event.content.membership === "invite") {
+            console.log(`${event.state_key} was invited to ${event.room_id}`);
+            if (event.state_key === this.bridge.getBot().getUserId()) {
+                console.log(`Accepting invite.`);
+                this.bridge.getIntent(event.state_key)
+                    .join(event.room_id);
+            }
+        }else {
+            console.log(`Event ignored: ${event.type}`);
+            return;
+        }
+        return;
     }
 }
 
