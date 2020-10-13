4ec3050a2d53bec04a170f523ade1a89d4e202ff
Johan Geluk <johan@geluk.io>
2020-10-13 17:33:44 +0200

    Test fetching objects from database

--- entry.ts
+++ entry.ts
@@ -4,6 +4,34 @@ import MatrixBridge from "./src/MatrixBridge"
 import MatrixEventHandler from "./src/MatrixEventHandler";
 import WebHook from "./src/WebHook";
 
+import * as knex from "knex"
+import User from "./src/models/User";
+
+function toSnakeCase(value: string): string {
+    return value.replace(/[A-Z]/g, c => `_${c.toLowerCase()}`);
+}
+
+const config: knex.Config = {
+    "client": "sqlite3",
+    "connection": {
+        "filename": "appservice-db.sqlite",
+    },
+    "wrapIdentifier": (value, origImpl, queryContext) => origImpl(toSnakeCase(value)),
+    "useNullAsDefault": true // Required for SQLite support
+}
+
+console.log(`Running in ${process.cwd()}`)
+
+const db = knex(config);
+
+const fn = async () => {
+    const u = await db.select().from<User>("user");
+    console.log(u);
+}
+
+fn();
+
+
 const appService = MatrixBridge.create();
 
 appService.registerHandler(MatrixEventHandler.message((event, bridge) => {
