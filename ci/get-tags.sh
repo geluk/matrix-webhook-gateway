06f61d919d58e7a094eaa60dfa6c83ce5feb6c5a
Johan Geluk <johan@geluk.io>
2020-10-14 22:52:56 +0200

    Add get-tags script for CI

---
+++ ci/get-tags.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+version="$1"
+
+
+function print_version {
+    if [[ -z "$1" ]]; then
+        echo 0
+    else
+        echo $1
+    fi
+}
+
+tag="$(git describe --tags)"
+tag_regex='^([[:digit:]]+)(.([[:digit:]]+))?(.([[:digit:]]+))?(-[1-9][[:digit:]]*-g[a-f0-9]{6})?.?$'
+
+if [[ $tag =~ $tag_regex ]]; then
+    case $version in
+    "--major")
+        print_version "${BASH_REMATCH[1]}"
+        ;;
+    esac
+    case $version in
+    "--minor")
+        print_version "${BASH_REMATCH[3]}"
+        ;;
+    esac
+    case $version in
+    "--bugfix")
+        print_version "${BASH_REMATCH[5]}"
+        ;;
+    esac
+else
+    >&2 echo "Could not retrieve tag"
+    exit 1
+fi
