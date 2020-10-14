80a2c9539b5f4d2b526e71d433b9f13cdcf332cf
Johan Geluk <johan@geluk.io>
2020-10-14 21:58:13 +0200

    Suppress non-error Matrix log messages

--- entry.ts
+++ entry.ts
@@ -1,14 +1,17 @@
+import { Logging as MatrixLogger } from 'matrix-appservice-bridge';
+
 import MatrixBridge from './src/bridge/MatrixBridge';
 import Database from './src/repositories/Database';
 import WebHookListener from './src/WebHookListener';
 import WebHookService from './src/WebHookService';
 
+MatrixLogger.default.configure({
+  console: 'error',
+  maxFiles: 1,
+});
 
 const whs = new WebHookService(
   new MatrixBridge(),
   new Database(),
 );
 whs.start();
-
-const webHook = new WebHookListener();
-webHook.start();
