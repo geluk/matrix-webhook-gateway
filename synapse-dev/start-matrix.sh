0d8b6db36f2b6c324b9de1eae949e11a0693245c
Johan Geluk <johan@geluk.io>
2020-10-16 14:22:09 +0200

    Add development configuration file

--- synapse-dev/start-matrix.sh
+++ synapse-dev/start-matrix.sh
@@ -4,7 +4,10 @@ if [ ! -e "synapse-data/homeserver.db" ]; then
     echo "Copying homeserver.db from template"
     cp template/homeserver.db synapse-data/homeserver.db
 fi
-
+if [ ! -e "../webhook-appservice.yaml" ]; then
+    echo "Copying webhook-appservice.yaml from template"
+    cp template/webhook-appservice.yaml ../webhook-appservice.yaml
+fi
 export SYNAPSE_UID="$(id -u)"
 export SYNAPSE_GID="$(id -g)"
 
