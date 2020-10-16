32ebccbb9d5693a2eae9bd131a2d3a2a0fa2144e
Johan Geluk <johan@geluk.io>
2020-10-16 11:05:33 +0200

    Hide log messages coming from matrix-js-sdk

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -5,5 +5,10 @@ const logger = new Logger({
   displayFunctionName: false,
   displayFilePath: 'hidden',
   minLevel: 'trace',
+  // This is a bit of a hack, but we need to enable it to prevent matrix-js-sdk
+  // from logging event details directly to the console. It may be possible to
+  // attach your own logger to it though; in that case this can be disabled.
+  // See: https://github.com/matrix-org/matrix-js-sdk/blob/develop/src/logger.js
+  overwriteConsole: true,
 });
 export default logger;
