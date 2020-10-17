b7c264a468b139ed3b84569a5f158444f1182a45
Johan Geluk <johan@geluk.io>
2020-10-17 23:28:27 +0200

    Implement argument parsing

--- entry.ts
+++ entry.ts
@@ -2,8 +2,27 @@ import { Logging as MatrixLogger } from 'matrix-appservice-bridge';
 
 import ConfigReader from './src/configuration/ConfigReader';
 import Database from './src/repositories/Database';
-import logger from './src/util/logger';
+import logger, { configureLogger } from './src/util/logger';
 import WebHookService from './src/WebHookService';
+
+// eslint-disable-next-line import/order
+import yargs = require('yargs/yargs');
+
+const { argv } = yargs(process.argv.slice(2))
+  .version(false)
+  .usage('$0 [options]')
+  .alias('c', 'config-file')
+  .string('c')
+  .nargs('c', 1)
+  .default('c', './webhook-appservice.yaml')
+  .describe('c', 'Path to the configuration file')
+  .count('v')
+  .alias('v', 'verbose')
+  .describe('v', 'Log verbosity, repeat multiple times to raise')
+  .help('h')
+  .alias('h', 'help');
+
+configureLogger(argv.verbose);
 
 MatrixLogger.default.configure({
   console: 'error',
@@ -17,7 +36,7 @@ process.on('unhandledRejection', (error) => {
   process.exit(1);
 });
 
-const config = ConfigReader.loadConfig('webhook-appservice.yaml');
+const config = ConfigReader.loadConfig(argv.c);
 if (typeof config === 'undefined') {
   logger.fatal('Could not load configuration file, application will now exit');
   process.exit(1);
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -5,10 +5,32 @@ const logger = new Logger({
   displayFunctionName: false,
   displayFilePath: 'hidden',
   minLevel: 'trace',
-  // This is a bit of a hack, but we need to enable it to prevent matrix-js-sdk
-  // from logging event details directly to the console. It may be possible to
-  // attach your own logger to it though; in that case this can be disabled.
-  // See: https://github.com/matrix-org/matrix-js-sdk/blob/develop/src/logger.js
-  overwriteConsole: true,
 });
 export default logger;
+
+type LogLevel = 'silly' | 'debug' | 'trace' | 'info' | 'warn' | 'error' | 'fatal';
+function getLevel(verbosity: number): LogLevel {
+  switch (verbosity) {
+    case 0:
+      return 'info';
+    case 1:
+      return 'debug';
+    case 2:
+      return 'trace';
+    default:
+      return 'silly';
+  }
+}
+
+export function configureLogger(verbosity: number): void {
+  logger.setSettings({
+    minLevel: getLevel(verbosity),
+    // This is a bit of a hack, but we need to enable it to prevent matrix-js-sdk
+    // from logging event details directly to the console. It may be possible to
+    // attach your own logger to it though; in that case this can be disabled.
+    // See: https://github.com/matrix-org/matrix-js-sdk/blob/develop/src/logger.js
+    // Should only be done after yargs has finished parsing arguments, because it
+    // wants to write its feedback to the console.
+    overwriteConsole: true,
+  });
+}
