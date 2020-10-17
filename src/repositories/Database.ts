3a085685bdacd10370bd87f2c87f22a321449426
Johan Geluk <johan@geluk.io>
2020-10-17 15:38:02 +0200

    Ensure initial migration succeeds

--- src/repositories/Database.ts
+++ src/repositories/Database.ts
@@ -35,7 +35,18 @@ export default class Database {
   }
 
   public async migrate(): Promise<void> {
-    const migrations = await this._knex.migrate.status();
+    let migrations: number;
+    try {
+      migrations = await this._knex.migrate.status();
+    } catch {
+      // This may have failed if we haven't executed the initial migration yet.
+      // Let's try that first. If something else is wrong, this will also throw
+      // and stop the migration process.
+      await this._knex.migrate.up();
+      logger.info('Created initial database structure');
+      // If it succeeded, we should now be able to request the migration status.
+      migrations = await this._knex.migrate.status();
+    }
     if (migrations === 0) {
       logger.debug('There are no pending migrations');
       return;
