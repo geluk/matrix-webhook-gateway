fb33670dde3f8fddc645eddc7a104e1a284df2c0
Johan Geluk <johan@geluk.io>
2020-10-18 12:47:55 +0200

    Create synapse-data directory if it does not exist

--- local-dev/start-matrix.sh
+++ local-dev/start-matrix.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+if [ ! -d "synapse-data" ]; then
+    echo "Creating synapse-data directory"
+    mkdir synapse-data
+fi
 if [ ! -e "synapse-data/homeserver.db" ]; then
     echo "Copying homeserver.db from template"
     cp template/homeserver.db synapse-data/homeserver.db
