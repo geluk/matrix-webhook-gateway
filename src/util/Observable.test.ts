e8e3d191feb9a8fd4dbc2b8b71c9b07c18ea0c7f
Johan Geluk <johan@geluk.io>
2020-10-20 17:39:45 +0200

    Make Observable tests async

--- src/util/Observable.test.ts
+++ src/util/Observable.test.ts
@@ -1,38 +1,38 @@
 import Observable from './Observable';
 
-test('calls every handler once', () => {
+test('calls every handler once', async () => {
   const obs = new Observable<string>();
   const h1 = jest.fn();
   const h2 = jest.fn();
   obs.observe(h1);
   obs.observe(h2);
 
-  obs.notify('message');
+  await obs.notify('message');
 
   expect(h1).toBeCalledTimes(1);
   expect(h2).toBeCalledTimes(1);
 });
 
-test('forwards message to observer', () => {
+test('forwards message to observer', async () => {
   const obs = new Observable<string>();
   const h = jest.fn();
   obs.observe(h);
 
-  obs.notify('message');
+  await obs.notify('message');
 
   expect(h).toBeCalledWith('message');
 });
 
-test('does not call handler when not notified', () => {
+test('does not call handler when not notified', async () => {
   const obs = new Observable<string>();
   const h = jest.fn();
 
-  obs.observe(h);
+  await obs.observe(h);
 
   expect(h).toBeCalledTimes(0);
 });
 
-test('does not call remaining handlers if one throws', () => {
+test('does not call remaining handlers if one throws', async () => {
   const obs = new Observable<string>();
   const before = jest.fn();
   const after = jest.fn();
@@ -44,7 +44,11 @@ test('does not call remaining handlers if one throws', () => {
   obs.observe(fail);
   obs.observe(after);
 
-  obs.notify('message');
+  try {
+    await obs.notify('message');
+  } catch (error) {
+    // Ignored
+  }
 
   expect(before).toBeCalledTimes(1);
   expect(after).toBeCalledTimes(0);
