5e4a8861d8e9be235417c595316f0d60418fadce
Johan Geluk <johan@geluk.io>
2021-08-18 16:20:17 +0200

    Simplify build script

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -8,7 +8,5 @@ fail() {
 stat out/webhook-gateway > /dev/null 2>&1 || \
     fail "No build artifacts. Please build the application first."
 
-stat out/dist 2> /dev/null && rm -r out/dist 
-mkdir out/dist
+cp -r templates node_modules out/webhook-gateway
 
-cp -r templates node_modules out/webhook-gateway/* out/dist
