c02d9593f6c437731c53551180e6e448ad41072b
Johan Geluk <johan@geluk.io>
2021-08-28 20:27:13 +0200

    Install prettier
    We don't include it in the build pipeline yet.
    Instead, we'll wait until all files have been formatted.

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Left over from tslint configuration, I'd rather not change this because
+  // it would lead to massive changes in every file.
+  singleQuote: true,
+  // See above. In addition, enforcing trailing commas everywhere makes diffs
+  // a bit nicer to look at.
+  trailingComma: 'all',
+};
