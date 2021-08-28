609e31d69ee20249bb7110550ae62391ae697e7b
Johan Geluk <johan@geluk.io>
2021-08-28 22:03:06 +0200

    Install eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,37 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: "tsconfig.json",
-    },
-    plugins: [
-        "@typescript-eslint",
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
+    'prettier',
+  ],
+  env: {
+    jest: true,
+  },
+  rules: {
+    'class-methods-use-this': ['off'],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allowAfterThis: true,
+      },
     ],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "airbnb-typescript/base",
+    'no-restricted-syntax': ['off'],
+    'no-unused-vars': ['off'],
+    'no-await-in-loop': ['off'],
+    'prefer-destructuring': ['off'],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
     ],
-    env: {
-        "jest": true,
-    },
-    rules: {
-        "class-methods-use-this": ["off"],
-        "no-underscore-dangle": ["error", {
-                "allowAfterThis": true,
-            }],
-        "no-restricted-syntax": ["off"],
-        "no-unused-vars": ["off"],
-        "no-await-in-loop": ["off"],
-        "prefer-destructuring": ["off"],
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            "argsIgnorePattern": "^_",
-        }],
-    },
-    ignorePatterns: [
-        "plugins"
-    ],
+  },
+  ignorePatterns: ['plugins'],
 };
