77a125def25fb44390d3a866b4c3f89359c3b639
Johan Geluk <johan@geluk.io>
2021-08-30 20:40:45 +0200

    Use mediumtext for image_cache table

--- migrations/20210819204408_image_caching.ts
+++ migrations/20210819204408_image_caching.ts
@@ -2,19 +2,12 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('image_cache', (table) => {
-    table.string('url_hash', 128)
-      .primary()
-      .notNullable();
-    table.string('original_url', 1024)
-      .notNullable();
-    table.string('matrix_url', 255)
-      .notNullable();
-    table.timestamp('last_retrieved')
-      .notNullable();
-    table.string('content_hash', 128)
-      .notNullable();
-    table.json('cache_details')
-      .notNullable();
+    table.string('url_hash', 128).primary().notNullable();
+    table.string('original_url', 1024).notNullable();
+    table.string('matrix_url', 255).notNullable();
+    table.timestamp('last_retrieved').notNullable();
+    table.string('content_hash', 128).notNullable();
+    table.text('cache_details', 'mediumtext').notNullable();
   });
 }
 
