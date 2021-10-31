e1b1874bca0e20e73a171876bb8dedb18895f334
Johan Geluk <johan@geluk.io>
2021-10-31 13:33:17 +0100

    Extract plugin loading logic to separate class

--- src/webhooks/PluginCollection.ts
+++ src/webhooks/PluginCollection.ts
@@ -1,27 +1,18 @@
 import fs from 'fs';
 import path from 'path';
-import hasha from 'hasha';
-import * as tts from 'ttypescript';
-import { is } from 'typescript-is';
 import watch from 'node-watch';
-import isTransformer from 'typescript-is/lib/transform-inline/transformer';
-import { ModuleKind, ScriptTarget } from 'ttypescript';
-import { Logger } from 'tslog';
 
 import logger from '../util/logger';
 import * as v1 from '../pluginApi/v1';
 import * as v2 from '../pluginApi/v2';
 import WebhooksConfiguration from '../configuration/WebhooksConfiguration';
 import MatrixBridge from '../bridge/MatrixBridge';
-
-type Plugin =
-  | v1.WebhookPlugin
-  | v2.PluginBase;
-
-const WORK_DIR = './plugins/__workdir';
+import { Plugin, PluginLoader } from './PluginLoader';
 
 export default class PluginCollection {
   private plugins: Record<string, Plugin> = {};
+
+  private loader: PluginLoader;
 
   private loaded = false;
 
@@ -31,10 +22,11 @@ export default class PluginCollection {
 
   public constructor(
     private config: WebhooksConfiguration,
-    private bridge: MatrixBridge,
+    private bridge: MatrixBridge | undefined,
   ) {
     this.pluginDirectory = path.resolve(this.config.plugin_directory);
     this.cacheDirectory = path.resolve(this.config.plugin_cache_directory);
+    this.loader = new PluginLoader(this.config.plugin_cache_directory);
   }
 
   public async load(): Promise<void> {
@@ -43,13 +35,16 @@ export default class PluginCollection {
     }
     if (!fs.existsSync(this.pluginDirectory)) {
       logger.warn(`Plugin directory '${this.pluginDirectory}' does not exist. `
-      + 'No plugins will be loaded.');
+        + 'No plugins will be loaded.');
       return;
     }
 
     for await (const file of this.walk(this.pluginDirectory)) {
       if (file.endsWith('.ts')) {
-        await this.loadPlugin(`${file}`);
+        const loadResult = await this.loader.loadPlugin(file, this.bridge);
+        if (loadResult) {
+          this.plugins[loadResult.identifier] = loadResult.plugin;
+        }
       }
     }
 
@@ -68,7 +63,10 @@ export default class PluginCollection {
     }, async (evt, name) => {
       if (evt === 'update' && name) {
         logger.info(`Plugin ${name} was updated, reloading`);
-        this.loadPlugin(name);
+        const loadResult = await this.loader.loadPlugin(name, this.bridge);
+        if (loadResult) {
+          this.plugins[loadResult.identifier] = loadResult.plugin;
+        }
       }
     });
 
@@ -80,7 +78,7 @@ export default class PluginCollection {
   }
 
   public async apply(body: unknown, type: string):
-  Promise<v1.WebhookMessage | v2.WebhookMessage | undefined> {
+    Promise<v1.WebhookMessage | v2.WebhookMessage | undefined> {
     const plugin = this.plugins[type];
     return plugin.transform(body);
   }
@@ -110,116 +108,5 @@ export default class PluginCollection {
     }
   }
 
-  private async loadPlugin(pluginPath: string) {
-    const source = fs.readFileSync(pluginPath, 'utf8');
-    const hash = hasha(source, { algorithm: 'sha256' });
-    const cacheFile = `${this.cacheDirectory}/${hash}.js`;
 
-    if (!fs.existsSync(this.cacheDirectory)) {
-      fs.mkdirSync(this.cacheDirectory);
-    }
-
-    if (fs.existsSync(cacheFile)) {
-      logger.silly(`Cached plugin found for ${pluginPath}`);
-    } else {
-      logger.debug(`Compiling plugin: ${pluginPath}`);
-      this.compilePlugin(source, hash, cacheFile);
-    }
-    logger.debug(`Loading plugin ${path.basename(pluginPath)} from ${path.basename(cacheFile)}`);
-
-    // If this looks a bit like a hack, that's because it very much is. In order
-    //  for 'typescript-is' imports to work, the source file must be in a child
-    // directory of our application when we load it, so we temporarily copy
-    // it there, load it, then remove it again.
-    this.ensureWorkDirExists();
-    const importFile = path.resolve(`${WORK_DIR}/${hash}.js`);
-    fs.copyFileSync(cacheFile, importFile);
-
-    let pluginFile;
-    let pluginContainer;
-    try {
-      // There's no getting around using require() here, as we need to dynamically
-      // load the plugin.
-      // eslint-disable-next-line
-      pluginFile = require(importFile);
-      pluginContainer = pluginFile.default;
-    } catch (error) {
-      logger.error('Error loading plugin: ', error);
-      return;
-    } finally {
-      fs.unlinkSync(importFile);
-    }
-
-    if (pluginContainer.prototype instanceof v2.PluginBase) {
-      if (!is<{format: string}>(pluginFile)) {
-        logger.warn(`Plugin ${pluginPath} does not have a format field`);
-        return;
-      }
-
-      const pluginLogger = this.createLogger(pluginFile.format);
-
-      // eslint-disable-next-line new-cap
-      const instance = new pluginContainer(pluginLogger, this.bridge);
-      await instance.init();
-      this.plugins[pluginFile.format] = instance;
-      return;
-    }
-
-    if (is<v1.WebhookPlugin>(pluginContainer)) {
-      if (!pluginContainer.format.match(/^[a-z0-9_]+$/)) {
-        logger.warn(`Invalid plugin name: ${pluginContainer.format}`);
-        return;
-      }
-
-      if (pluginContainer.init) {
-        pluginContainer.init();
-      }
-      this.plugins[pluginContainer.format] = pluginContainer;
-      return;
-    }
-
-    logger.warn(`Unknown plugin type: ${pluginPath}`);
-    logger.debug('Plugin container:', pluginContainer);
-  }
-
-  private createLogger(pluginName: string): Logger {
-    return logger.getChildLogger({
-      name: `plg-${pluginName}`,
-    });
-  }
-
-  private compilePlugin(source: string, hash: string, cachePath: string) {
-    const sourcePath = `${WORK_DIR}/${hash}.ts`;
-
-    // Just as before, we need to place the file somewhere in a child directory
-    // of our application before we compile it, otherwise  `typescript-is`
-    // won't desugar any of its is<T>() calls in the plugin file.
-    this.ensureWorkDirExists();
-    fs.writeFileSync(sourcePath, source);
-    const prog = tts.createProgram([sourcePath], {
-      strictNullChecks: true,
-      target: ScriptTarget.ES2016,
-      module: ModuleKind.CommonJS,
-    });
-
-    prog.emit(undefined, (_name, data) => {
-      fs.writeFileSync(cachePath, data, {
-        flag: 'w',
-      });
-    }, undefined, undefined, {
-      before: [isTransformer(prog, {
-        functionBehavior: 'ignore',
-      })],
-    });
-
-    fs.unlinkSync(sourcePath);
-  }
-
-  private ensureWorkDirExists() {
-    if (!fs.existsSync(WORK_DIR)) {
-      fs.mkdirSync(WORK_DIR, {
-        recursive: true,
-      });
-    }
-  }
 }
---
+++ src/webhooks/PluginLoader.ts
@@ -0,0 +1,151 @@
+import fs from 'fs';
+import path from 'path';
+import hasha from 'hasha';
+import * as tts from 'ttypescript';
+import { is } from 'typescript-is';
+import isTransformer from 'typescript-is/lib/transform-inline/transformer';
+import { ModuleKind, ScriptTarget } from 'ttypescript';
+import { Logger } from 'tslog';
+
+import logger from '../util/logger';
+import * as v1 from '../pluginApi/v1';
+import * as v2 from '../pluginApi/v2';
+import MatrixBridge from '../bridge/MatrixBridge';
+
+const WORK_DIR = './plugins/__workdir';
+
+export interface LoadResult {
+  identifier: string,
+  plugin: Plugin,
+}
+
+export type Plugin =
+  | v1.WebhookPlugin
+  | v2.PluginBase;
+
+export class PluginLoader {
+
+  public constructor(
+    private cacheDirectory: string,
+  ) {
+
+  }
+
+  public async loadPlugin(pluginPath: string, bridge: MatrixBridge | undefined): Promise<LoadResult | undefined> {
+    const source = fs.readFileSync(pluginPath, 'utf8');
+    const hash = hasha(source, { algorithm: 'sha256' });
+    const cacheFile = `${this.cacheDirectory}/${hash}.js`;
+
+    if (!fs.existsSync(this.cacheDirectory)) {
+      fs.mkdirSync(this.cacheDirectory);
+    }
+
+    if (fs.existsSync(cacheFile)) {
+      logger.silly(`Cached plugin found for ${pluginPath}`);
+    } else {
+      logger.debug(`Compiling plugin: ${pluginPath}`);
+      this.compilePlugin(source, hash, cacheFile);
+    }
+    logger.debug(`Loading plugin ${path.basename(pluginPath)} from ${path.basename(cacheFile)}`);
+
+    // If this looks a bit like a hack, that's because it very much is. In order
+    //  for 'typescript-is' imports to work, the source file must be in a child
+    // directory of our application when we load it, so we temporarily copy
+    // it there, load it, then remove it again.
+    this.ensureWorkDirExists();
+    const importFile = path.resolve(`${WORK_DIR}/${hash}.js`);
+    fs.copyFileSync(cacheFile, importFile);
+
+    let pluginFile;
+    let pluginContainer;
+    try {
+      // There's no getting around using require() here, as we need to dynamically
+      // load the plugin.
+      // eslint-disable-next-line
+      pluginFile = require(importFile);
+      pluginContainer = pluginFile.default;
+    } catch (error) {
+      logger.error('Error loading plugin: ', error);
+      return undefined;
+    } finally {
+      fs.unlinkSync(importFile);
+    }
+
+    if (pluginContainer.prototype instanceof v2.PluginBase) {
+      if (!is<{ format: string }>(pluginFile)) {
+        logger.warn(`Plugin ${pluginPath} does not have a format field`);
+        return undefined;
+      }
+
+      const pluginLogger = this.createLogger(pluginFile.format);
+
+      // eslint-disable-next-line new-cap
+      const instance = new pluginContainer(pluginLogger, bridge);
+      await instance.init();
+      return {
+        identifier: pluginFile.format,
+        plugin: instance
+      }
+    }
+
+    if (is<v1.WebhookPlugin>(pluginContainer)) {
+      if (!pluginContainer.format.match(/^[a-z0-9_]+$/)) {
+        logger.warn(`Invalid plugin name: ${pluginContainer.format}`);
+        return undefined;
+      }
+
+      if (pluginContainer.init) {
+        pluginContainer.init();
+      }
+      return {
+        identifier: pluginContainer.format,
+        plugin: pluginContainer,
+      }
+    }
+
+    logger.warn(`Unknown plugin type: ${pluginPath}`);
+    logger.debug('Plugin container:', pluginContainer);
+    return undefined;
+  }
+
+  private createLogger(pluginName: string): Logger {
+    return logger.getChildLogger({
+      name: `plg-${pluginName}`,
+    });
+  }
+
+  private compilePlugin(source: string, hash: string, cachePath: string) {
+    const sourcePath = `${WORK_DIR}/${hash}.ts`;
+
+    // Just as before, we need to place the file somewhere in a child directory
+    // of our application before we compile it, otherwise  `typescript-is`
+    // won't desugar any of its is<T>() calls in the plugin file.
+    this.ensureWorkDirExists();
+    fs.writeFileSync(sourcePath, source);
+    const prog = tts.createProgram([sourcePath], {
+      strictNullChecks: true,
+      target: ScriptTarget.ES2016,
+      module: ModuleKind.CommonJS,
+    });
+
+    prog.emit(undefined, (_name, data) => {
+      fs.writeFileSync(cachePath, data, {
+        flag: 'w',
+      });
+    }, undefined, undefined, {
+      before: [isTransformer(prog, {
+        functionBehavior: 'ignore',
+      })],
+    });
+
+    fs.unlinkSync(sourcePath);
+  }
+
+  private ensureWorkDirExists() {
+    if (!fs.existsSync(WORK_DIR)) {
+      fs.mkdirSync(WORK_DIR, {
+        recursive: true,
+      });
+    }
+  }
+}
